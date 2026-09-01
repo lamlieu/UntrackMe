3d4636af26b2a26239d29b75bec9d409dbdf6ba3
lamlieu <contact.lamlieu@gmail.com>
2026-08-31 22:14:56 -0700

    Add support to sharing links to the app and remove YT and IG tracker query params

--- app/src/main/java/io/github/lamlieu/untrackme/MainActivity.kt
+++ app/src/main/java/io/github/lamlieu/untrackme/MainActivity.kt
@@ -2,16 +2,19 @@
 
 package io.github.lamlieu.untrackme
 
+import android.content.Intent.ACTION_SEND
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,23 +22,41 @@ import io.github.lamlieu.untrackme.ui.theme.UntrackMeTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+  private val isTextShareIntent: Boolean
+    get() = intent?.action == ACTION_SEND && intent?.type == "text/plain"
+
+  private val viewModel: MainViewModel by viewModels()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
+
+    if (isTextShareIntent) {
+      viewModel.processIntent(intent)
+    }
+
     setContent {
-      UntrackMeTheme {
-        Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-          CenterAlignedTopAppBar(title = {
-            Text(text = stringResource(R.string.app_name))
-          })
-        }) { innerPadding ->
-          OverviewScreen(
-            modifier = Modifier
-              .fillMaxSize()
-              .padding(innerPadding)
-          )
-        }
+      if (isTextShareIntent) {
+        SharesheetHandler(viewModel = viewModel, onComplete = { finish() })
+      } else {
+        Overview()
+      }
+    }
+  }
+
+  @Composable
+  fun Overview() {
+    UntrackMeTheme {
+      Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+        CenterAlignedTopAppBar(title = {
+          Text(text = stringResource(R.string.app_name))
+        })
+      }) { innerPadding ->
+        OverviewScreen(
+          modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+        )
       }
     }
   }
---
+++ app/src/main/java/io/github/lamlieu/untrackme/MainViewModel.kt
@@ -0,0 +1,49 @@
+package io.github.lamlieu.untrackme
+
+import android.content.Intent
+import androidx.core.net.toUri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+  // `replay = 1` resolves race condition for if processIntent finishes first before collecting.
+  private val _cleanedLink = MutableSharedFlow<String>(replay = 1)
+  val cleanedLink = _cleanedLink.asSharedFlow()
+
+  fun processIntent(intent: Intent?) {
+    if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+      val link = intent.getStringExtra(Intent.EXTRA_TEXT)
+      if (link != null) {
+        viewModelScope.launch {
+          _cleanedLink.emit(removeTrackingParams(link))
+        }
+      }
+    }
+  }
+
+  private fun removeTrackingParams(link: String): String {
+    val uri = link.toUri()
+    val newUri = uri.buildUpon().clearQuery()
+    for (param in uri.queryParameterNames) {
+      if (!trackingParams.contains(param)) {
+        newUri.appendQueryParameter(param, uri.getQueryParameter(param))
+      }
+    }
+    return newUri.build().toString()
+  }
+
+  companion object {
+    private val trackingParams: Set<String> = setOf(
+      // Instagram
+      "igsi",
+      // YouTube
+      "si",
+    )
+  }
+}
