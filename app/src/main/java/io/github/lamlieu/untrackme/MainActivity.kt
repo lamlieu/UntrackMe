736ddc39e020124714e16ab3cacefaa2c7bb6f1e
lamlieu <contact.lamlieu@gmail.com>
2026-09-02 23:31:37 -0700

    Update background color, and add constrasting colors and shape to InstructionCard

--- app/src/main/java/io/github/lamlieu/untrackme/MainActivity.kt
+++ app/src/main/java/io/github/lamlieu/untrackme/MainActivity.kt
@@ -13,10 +13,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.lamlieu.untrackme.ui.theme.UntrackMeTheme
 
@@ -47,16 +49,18 @@ class MainActivity : ComponentActivity() {
   @Composable
   fun Instructions() {
     UntrackMeTheme {
-      Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        CenterAlignedTopAppBar(title = {
-          Text(text = stringResource(R.string.main_activity_top_app_bar_title))
-        })
-      }) { innerPadding ->
-        InstructionScreen(
-          modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)
-        )
+      Surface(tonalElevation = 5.dp) {
+        Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+          CenterAlignedTopAppBar(title = {
+            Text(text = stringResource(R.string.main_activity_top_app_bar_title))
+          })
+        }) { innerPadding ->
+          InstructionScreen(
+            modifier = Modifier
+              .fillMaxSize()
+              .padding(innerPadding)
+          )
+        }
       }
     }
   }
